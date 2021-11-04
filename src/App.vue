507e308752b3a99c2516912365a45f8271e16828
rakib3004 <bsse1129@iit.du.ac.bd>
2021-11-04 11:03:25 +0600

    add trace tacker and its style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
- <h1>Hello World</h1>
+<div class="container">
+<Header title="Trace Tacker" />
+ </div>
 </template>
 
 <script>
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
 
 *{
   box-sizing: border-box;
