7b8a70dce72a8742f8c893db6e6285c5a445886c
rakib3004 <bsse1129@iit.du.ac.bd>
2021-11-04 11:32:06 +0600

    add tasks list and show them

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
 <div class="container">
 <Header title="Trace Tacker" />
+<Tasks :tasks="tasks" />
  </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header, 
+    Tasks,
   },
   data(){
     return{
---
+++ src/components/Tasks.vue
@@ -0,0 +1,17 @@
+<template>
+<header>
+    <div :key="task.id" v-for="task in tasks">
+    <h3>{{task.text}}</h3>
+    </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Tasks',
+  props:{
+      tasks: Array,
+  },
+}
+</script>
