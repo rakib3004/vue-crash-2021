4fe6163a05726baeee8a2e77bbe244a7713dc5cf
rakib3004 <bsse1129@iit.du.ac.bd>
2021-11-04 12:38:38 +0600

    add all task and their styles

---
+++ src/components/Task.vue
@@ -0,0 +1,52 @@
+<template>
+    <header>
+    <div :class="[task.reminder ? 'reminder': '', 'task']">
+        <h3>{{task.text}}
+        <i @click="onDelete(task.id)" class="fas fa-times">X</i>
+        </h3>
+        
+        <p>{{task.day}}</p>
+
+    </div>
+    </header>
+</template>
+
+<script>
+
+export default{
+    name: 'Task',
+    props:{
+        task: Object,
+    },
+    methods:{
+        onDelete(id){
+           this.$emit('delete-task', id)
+        }
+    }
+}
+</script>
+
+<style scope>
+
+.fas{
+    color: red;
+}
+.task{
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task.reminder{
+    border-left: 5px solid green;
+
+}
+
+.task h3{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+</style>
